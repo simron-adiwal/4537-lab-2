357a8de4f801da83d997e912068fe0ed9b854a9d
simron-adiwal <77468210+simron-adiwal@users.noreply.github.com>
2023-01-17 12:00:50 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// import the necessary modules.
+const express = require('express');
+
+
+// Create a new Express app and configure it to use the body-parser middleware.
+const app = express(); 
+app.use(express.json()); // read JSON BODY
+app.use(express.urlencoded({ extended: true })); // read URL encoded body
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+app.post('/chatbot', (req, res) => {
+	const message = req.body.message;
+	const number = message.match(/\d+/);
+	if (number) {
+		fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
+			res.json({
+				text: data
+			});
+		}).catch(error => {
+			res.json({
+				text: "Sorry, I couldn't find any information about that number."
+			});
+		});
+	} else {
+		res.json({
+			text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
+		});
+	}
+});
+
+// Start the server by calling app.listen() and specify a port for the server to listen on.
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
---
+++ script.js
@@ -0,0 +1,75 @@
+// JQUERY
+$(document).ready(function () {
+    $('#chatbot-form').submit(function (event) {
+      event.preventDefault();
+      const message = $('#chatbot-input').val();
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:3000/chatbot',
+        // url: 'https://nabil828-comp4537-w23-labs-lab2.onrender.com/chatbot',
+        data: { message: message },
+        success: function (response) {
+          let newMessage = $('<div>', { class: 'message' }).text(response.text);
+          let removeButton = $('<button>', { class: 'remove-button' }).text('Remove');
+          newMessage.append(removeButton);
+          $('#chat-history').append(newMessage);
+        }
+  
+      });
+      $(document).on('click', '.remove-button', function () {
+        $(this).parent().remove();
+      });
+  
+    });
+  });
+  
+  
+  // VANILLA JS
+  // document.addEventListener("DOMContentLoaded", function () {
+  //   document.getElementById('chatbot-form').addEventListener('submit', function (event) {
+  //     event.preventDefault();
+  //     const message = document.getElementById('chatbot-input').value;
+  //     fetch('http://localhost:3000/chatbot', {
+  //       // fetch('https://nabil828-comp4537-w23-labs-lab2.onrender.com/chatbot', {
+  //       method: 'POST',
+  //       body: JSON.stringify({ message: message }),
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     })
+  //       .then(response => response.json())
+  //       .then(response => {
+  //         console.log(response);
+  //         const newMessage = document.createElement('div');
+  //         newMessage.classList.add('message');
+  //         newMessage.innerHTML = response.text;
+  //         const removeButton = document.createElement('button');
+  //         removeButton.classList.add('remove-button');
+  //         removeButton.innerHTML = 'Remove';
+  //         newMessage.appendChild(removeButton);
+  //         document.getElementById('chat-history').appendChild(newMessage);
+  //       })
+  //       .catch(error => console.error('Error:', error));
+  //   });
+  //   document.addEventListener('click', function (event) {
+  //     if (event.target.classList.contains('remove-button')) {
+  //       event.target.parentNode.remove();
+  //     }
+  //   });
+  // });
+
+
+
+
+
+
+
+// function getNumberInfo(number, format) {
+// 	const API_URL = `http://numbersapi.com/${number}/${format}`;
+// 	fetch(API_URL).then(response => response.text()).then(data => {
+// 		console.log(data);
+// 	}).catch(error => {
+// 		console.log(error);
+// 	});
+// }
+// getNumberInfo(42, 'trivia');
